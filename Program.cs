fb418414ce210dbfdda6183cc6d63d8bf670b0fa
mailz <sergey4istyak@yandex.ru>
2023-04-01 13:36:49 +0400

    Решил задачи 1 и 2

--- Program.cs
+++ Program.cs
@@ -1,13 +1,18 @@
 ﻿
-/*
-Задача 19
-Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+/* 
+
+ДОМАШНЕЕ ЗАДАНИЕ от 01.04.2023 
+
+Задача 19: Напишите программу, которая принимает на вход пятизначное число и 
+проверяет, является ли оно палиндромом.
 14212 -> нет
 12821 -> да
 23432 -> да
-*/
+------------
+РЕШЕНИЕ 
+------------------- 
 
-Console.WriteLine("Введите число: ");
+Console.WriteLine("Введите число и я проверю, является ли он палиндромом: ");
 string number = Console.ReadLine();
 int len = number.Length;
 
@@ -15,58 +20,61 @@ if (len == 5)
 {
     if (number[0] == number[4] && number[1] == number[3])
     {
-        Console.WriteLine($"{number} - Палиндром");
+        Console.WriteLine($"{number} -> Да, это  палиндром");
     }
     else
     {
-        Console.WriteLine($"{number} - НЕ палиндром");
+        Console.WriteLine($"{number} -> Нет, это не палиндром");
     }
 }
 else
 {
-    Console.WriteLine($"ОШИБКА: {number} - не является пятизначным");
+    Console.WriteLine($"Это число {number} не пятизначное.");
 }
 
-
-/*
-Задача 21 (branch task_2)
-Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+---------------------------------------
+ 
+Задача 21: Напишите программу, которая принимает на вход координаты двух точек и 
+находит расстояние между ними в 3D пространстве.
 A (3,6,8); B (2,1,-7), -> 15.84
 A (7,-5, 0); B (1,-1,9) -> 11.53
+
+РЕШЕНИЕ:
+-------------------
 */
 
-int x1 = ReadInt("Введите координату X первой точки: ");
-int y1 = ReadInt("Введите координату Y первой точки: ");
-int z1 = ReadInt("Введите координату Z первой точки: ");
-int x2 = ReadInt("Введите координату X второй точки: ");
-int y2 = ReadInt("Введите координату Y второй точки: ");
-int z2 = ReadInt("Введите координату Z второй точки: ");
+Console.WriteLine("Задайте координату X1 первой точки: ");
+int x1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Задайте координату Y1 первой точки: ");
+int y1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Задайте координату Z1 первой точки: ");
+int z1 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Задайте координату X2 второй точки: ");
+int x2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Задайте координату Y2 второй точки: ");
+int y2 = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Задайте координату Z2 второй точки: ");
+int z2 = int.Parse(Console.ReadLine());
 
 int A = x2 - x1;
 int B = y2 - y1;
 int C = z1 - z2;
 
 double length = Math.Sqrt(A * A + B * B + C * C);
-Console.WriteLine($"Длинна отрезка {length}");
-
-
-// Функция ввода сообщения
-int ReadInt(string message)
-{
-    Console.Write(message);
-    return Convert.ToInt32(Console.ReadLine());
-}
-
-
-
-
+Console.WriteLine($"Расстояние между точками в 3D пространстве составляет -> {length}");
 
 /*
+
 Задача 23
 Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
 3 -> 1, 8, 27
 5 -> 1, 8, 27, 64, 125
-*/
+
 
 int number = ReadInt("Введите число N: ");
 
@@ -75,11 +83,9 @@ for (int i = 1; i <= number; i++)
     Console.Write($"{i*i*i} ");
 }
 
-
-
-// Функция ввода сообщения
 int ReadInt(string message)
 {
     Console.Write(message);
     return Convert.ToInt32(Console.ReadLine());
 }
+*/
